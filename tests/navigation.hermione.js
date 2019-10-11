412ed835d83044692ffc00758a5157b42705aa6d
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-10-12 00:23:50 +0300

    add navigation tests for folder table

--- tests/navigation.hermione.js
+++ tests/navigation.hermione.js
@@ -1,15 +1,31 @@
+const { assert } = require('chai');
+
 describe('navigation in table', () => {
-    it('should change folder content if click on folder', function () {
+    it('if click on folder in table chould change page', async function () {
+        // 2-ая строка таблицы (после шапки), 1-ая колонка -> директория
+        const folderLinkSelector = '.Table .Table-Row:nth-child(2) .Table-Cell:nth-child(1) .Link';
+        const breadcrumbFolderSelector = '.Breadcrumbs .Breadcrumbs-Item:last-child';
 
+        const folderName = await this.browser
+            .url('/')
+            .waitForExist(folderLinkSelector, 3000)
+            .getText(folderLinkSelector);
+
+        return this.browser
+            .click(folderLinkSelector)
+            .assertTexts(folderName, breadcrumbFolderSelector);
     });
 
-    it('should change folder content if click on folder, and then on next folder', function () {
 
-    });
-
-    
     it('should show file content if click on file', function () {
+        // допущение: в последней строке таблицы будет ссылка на файл
+        const fileLinkSelector = '.Table .Table-Row:last-child .Table-Cell:first-child .Link';
 
+        return this.browser
+            .url('/')
+            .waitForExist(fileLinkSelector, 3000)
+            .click(fileLinkSelector)
+            .assertExists('.FileEditor', 'File content shown');
     });
 })
 
