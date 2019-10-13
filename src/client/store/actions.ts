7dfafaeb1eb638551ed1b36a91ab1e8c31e5dcab
Alexandra Smirnova <smirnova.a.yu@gmail.com>
2019-10-13 15:32:48 +0300

    rewrite actions and actionsTypes to ts

--- src/client/store/actions.ts
+++ src/client/store/actions.ts
@@ -2,29 +2,30 @@ import {
     SET_DIRECTORY_CONTENT, SET_DIRECTORY_PATH, SET_REPOSITORIES, SET_CURRENT_REPOSITORY,
     SET_FILE_DATA, CLEAR_FILE_DATA
 } from "./actionTypes";
+import { ContentDirectoryData, FileContent } from "../types";
 
 
-export const setRepositories = (repos) => ({
+export const setRepositories = (repos: string[]) => ({
     type: SET_REPOSITORIES,
     payload: repos,
 })
 
-export const setCurrentRepository = (repo) => ({
+export const setCurrentRepository = (repo: string) => ({
     type: SET_CURRENT_REPOSITORY,
     payload: repo,
 })
 
-export const setDirectoryContent = (content) => ({
+export const setDirectoryContent = (content: ContentDirectoryData[]) => ({
     type: SET_DIRECTORY_CONTENT,
     payload: content,
 })
 
-export const setDirectoryPath = (path) => ({
+export const setDirectoryPath = (path: string) => ({
     type: SET_DIRECTORY_PATH,
     payload: path,
 })
 
-export const setFileData = (data) => ({
+export const setFileData = (data: FileContent) => ({
     type: SET_FILE_DATA,
     payload: data,
 })
